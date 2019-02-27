c7eea17d792c76352ebb831731d04a2130ef1375
Sergioalf <sealfarofa@ittepic.edu.mx>
2019-02-27 15:20:02 -0700

    Segundo  commit

--- schema.js
+++ schema.js
@@ -1,2 +1,25 @@
 var mongoose = requiere("mongoose");
 
+module.exports = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            requiered: true
+        },
+        author: {
+            type: String,
+            required:true
+        },
+        body: {
+            type: String,
+            requiered: true
+        },
+        comments: [{body: String, date: Date}],
+        date: {type: Date, default: Date.now},
+        hidden: {type: Boolean, default: false},
+        meta: {
+            votes: Number,
+            favs: Number 
+        }
+    }
+);
